1f3d575bf82c691b8e0efb0690f7c4f984739eda
Mark001 <1989329376@qq.com>
2018-08-10 15:45:07 +0800

    Mark- frst submit

---
+++ MarkTool/MarkTool.h
@@ -0,0 +1,14 @@
+//
+//  MarkTool.h
+//  Test
+//
+//  Created by Mark on 2018/8/10.
+//  Copyright © 2018年 Mark. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MarkTool : NSObject
++(void)logMessage:(NSString *)message;
+
+@end
---
+++ MarkTool/MarkTool.m
@@ -0,0 +1,17 @@
+//
+//  MarkTool.m
+//  Test
+//
+//  Created by Mark on 2018/8/10.
+//  Copyright © 2018年 Mark. All rights reserved.
+//
+
+#import "MarkTool.h"
+
+@implementation MarkTool
+
++(void)logMessage:(NSString *)message
+{
+    NSLog(@"logMessage=%@",logMessage);
+}
+@end
